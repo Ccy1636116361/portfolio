940d346d1fb9babd84681fd847c01a7ae8395d8f
Chris Young <ccy3691@gmail.com>
2024-04-07 12:44:11 -0400

    Update project-card.svelte
    dont render <a> if their is no link in the db.

--- apps/web/src/lib/components/project-card.svelte
+++ apps/web/src/lib/components/project-card.svelte
@@ -7,24 +7,46 @@
     export let imgUrl: string
 </script>
   
-  <a href={project.link} target="_blank" class="flex gap-2 max-w-lg">
-    <Card.Root>
-        <Card.Header class="md:min-h-60">
-            <Card.Title class="text-2xl">{project.title}</Card.Title>
-            <Card.Description>{project.description}</Card.Description>
-        </Card.Header>
-        <Card.Content class="md:min-h-80">
-            <img src={`${imgUrl}/api/files/${project.collectionId}/${project.id}/${project.thumbnail}?thumb=100x250`} alt={project.title}/>
-        </Card.Content>
-        <Card.Footer class="flex flex-wrap gap-2">
-        {#if project?.expand?.tags}
-            {#each project.expand?.tags as tag}
-                <Badge variant="default"><a href={tag.url} target="_blank">{tag.text}</a></Badge>
-            {/each}
-         {/if}
-        </Card.Footer>
-    </Card.Root>
-</a>
+    {#if project.link}
+        <a href={project.link} target="_blank" class="flex gap-2 max-w-lg">
+            <Card.Root>
+                <Card.Header class="md:min-h-60">
+                    <Card.Title class="text-2xl">{project.title}</Card.Title>
+                    <Card.Description>{project.description}</Card.Description>
+                </Card.Header>
+                <Card.Content class="md:min-h-80">
+                    <img src={`${imgUrl}/api/files/${project.collectionId}/${project.id}/${project.thumbnail}?thumb=100x250`} alt={project.title}/>
+                </Card.Content>
+                <Card.Footer class="flex flex-wrap gap-2">
+                {#if project?.expand?.tags}
+                    {#each project.expand?.tags as tag}
+                        <Badge variant="default"><a href={tag.url} target="_blank">{tag.text}</a></Badge>
+                    {/each}
+                {/if}
+                </Card.Footer>
+            </Card.Root>
+        </a>
+    {:else}
+        <div class="flex gap-2 max-w-lg">
+            <Card.Root>
+                <Card.Header class="md:min-h-60">
+                    <Card.Title class="text-2xl">{project.title}</Card.Title>
+                    <Card.Description>{project.description}</Card.Description>
+                </Card.Header>
+                <Card.Content class="md:min-h-80">
+                    <img src={`${imgUrl}/api/files/${project.collectionId}/${project.id}/${project.thumbnail}?thumb=100x250`} alt={project.title}/>
+                </Card.Content>
+                <Card.Footer class="flex flex-wrap gap-2">
+                {#if project?.expand?.tags}
+                    {#each project.expand?.tags as tag}
+                        <Badge variant="default"><a href={tag.url} target="_blank">{tag.text}</a></Badge>
+                    {/each}
+                {/if}
+                </Card.Footer>
+            </Card.Root>
+        </div>
+    {/if}
+
   
   
   
